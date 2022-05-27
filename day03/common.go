718fca30ca1b75ed5d0b077c1c8ee90f62ce6942
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2022-05-27 19:12:42 +1200

    use common part of day 3 solution in part 1

--- day03/common.go
+++ day03/common.go
@@ -1,10 +1,5 @@
 package day03
 
-import (
-//	"aoc2021/util"
-//	"fmt"
-)
-
 func split(input []string, pos int) (ones []string, zeroes []string) {
 	for _, l := range input {
 		if l[pos] == '1' {
--- day03/part1.go
+++ day03/part1.go
@@ -8,19 +8,12 @@ import (
 func Part1(inputFile string) string {
 	lines := util.ReadInput(inputFile)
 	dim := len(lines[0])
-	counters := make([]int, dim)
-	for _, l := range lines {
-		for pos := 0; pos < dim; pos++ {
-			if l[pos] == '1' {
-				counters[pos]++
-			}
-		}
-	}
 	var gamma, epsilon int
 	for pos := 0; pos < dim; pos++ {
 		gamma *= 2
 		epsilon *= 2
-		if counters[pos] > len(lines)/2 {
+		ones, zeroes := split(lines, pos)
+		if len(ones) > len(zeroes) {
 			gamma++
 		} else {
 			epsilon++
