3e21e34de827a29bb29ef36f69f3a3154d671110
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2022-09-25 15:49:56 +1300

    do another round of clean up and optimisation for day 22 solutions

--- day22/common.go
+++ day22/common.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 )
 
+// https://stackoverflow.com/a/53587770/284111
 func findNamedMatches(regex *regexp.Regexp, str string) map[string]string {
 	match := regex.FindStringSubmatch(str)
 
@@ -52,11 +53,6 @@ func getZ(c *cube) (int, int) {
 	return c.zmin, c.zmax
 }
 
-type dimension struct {
-	getMinMax  func(c *cube) (int, int)
-	stopPoints []int
-}
-
 type memo struct {
 	selection string
 	dd        int
@@ -64,37 +60,60 @@ type memo struct {
 
 var memoMap = map[memo]int64{}
 
-func sweep(selection []*cube, dd []dimension) int64 {
+func getStopPoints(selection []*cube, getMinMax func(c *cube) (int, int)) (stopPoints []int) {
+	m := map[int]struct{}{}
+	for _, c := range selection {
+		min, max := getMinMax(c)
+		m[min] = struct{}{}
+		m[max+1] = struct{}{}
+	}
+	for k := range m {
+		stopPoints = append(stopPoints, k)
+	}
+	sort.Ints(stopPoints)
+	return
+}
+
+func getSelectionKey(selection []*cube) string {
 	var b strings.Builder
 	for _, v := range selection {
 		b.WriteString(v.index + "|")
 	}
-	memoKey := memo{b.String(), len(dd)}
+	return b.String()
+}
+
+// https://work.njae.me.uk/2021/12/29/advent-of-code-2021-day-22/
+func sweep(selection []*cube, getMinMaxs [](func(c *cube) (int, int))) int64 {
+	memoKey := memo{getSelectionKey(selection), len(getMinMaxs)}
 	if c, ok := memoMap[memoKey]; ok {
 		return c
 	}
-	d := dd[0]
-	if len(dd) > 0 {
-		dd = dd[1:]
+
+	getMinMax := getMinMaxs[0]
+	if len(getMinMaxs) > 0 {
+		getMinMaxs = getMinMaxs[1:]
 	}
 
-	var count int64
-	var previousOn bool
-	var previousMultiplier int64
-	stopPoints := d.stopPoints
+	var (
+		count              int64
+		previousOn         bool
+		previousMultiplier int64
+		stopPoints         = getStopPoints(selection, getMinMax)
+	)
+
 	for i, v := range stopPoints {
 		newSelection := []*cube{}
 		for _, c := range selection {
-			min, max := d.getMinMax(c)
+			min, max := getMinMax(c)
 			if v >= min && v <= max {
 				newSelection = append(newSelection, c)
 			}
 		}
 		multiplier := int64(1)
-		if len(dd) > 0 {
-			multiplier = sweep(newSelection, dd)
+		if len(getMinMaxs) > 0 {
+			multiplier = sweep(newSelection, getMinMaxs)
 		}
-		on := len(newSelection) > 0 && newSelection[len(newSelection)-1].on || len(dd) > 0
+		on := len(newSelection) > 0 && newSelection[len(newSelection)-1].on || len(getMinMaxs) > 0
 		if on && i == len(stopPoints)-1 {
 			count += multiplier
 		}
@@ -112,40 +131,16 @@ func solve(inputFile string, part1 bool) string {
 	data := util.ReadInput(inputFile)
 	r := regexp.MustCompile(`^(?P<mode>on|off) x=(?P<xmin>-?\d+)\.\.(?P<xmax>-?\d+),y=(?P<ymin>-?\d+)\.\.(?P<ymax>-?\d+),z=(?P<zmin>-?\d+)\.\.(?P<zmax>-?\d+)$`)
 	cubes := []*cube{}
-	var xx, yy, zz []int
-	mxx := map[int]struct{}{}
-	myy := map[int]struct{}{}
-	mzz := map[int]struct{}{}
 	for i, s := range data {
 		m := findNamedMatches(r, s)
 		c := loadCube(m, fmt.Sprint(i))
 		if part1 && (c.xmin < -50 || c.xmax > 50 || c.ymin < -50 || c.ymax > 50 || c.zmin < -50 || c.zmax > 50) {
 			continue
 		}
-		mxx[c.xmin] = struct{}{}
-		mxx[c.xmax+1] = struct{}{}
-		myy[c.ymin] = struct{}{}
-		myy[c.ymax+1] = struct{}{}
-		mzz[c.zmin] = struct{}{}
-		mzz[c.zmax+1] = struct{}{}
 		cubes = append(cubes, c)
 	}
 
-	for k := range mxx {
-		xx = append(xx, k)
-	}
-	for k := range myy {
-		yy = append(yy, k)
-	}
-	for k := range mzz {
-		zz = append(zz, k)
-	}
-
-	sort.Ints(xx)
-	sort.Ints(yy)
-	sort.Ints(zz)
-
-	count := sweep(cubes, []dimension{{getX, xx}, {getY, yy}, {getZ, zz}})
+	count := sweep(cubes, [](func(c *cube) (int, int)){getX, getY, getZ})
 
 	return fmt.Sprint(count)
 }
