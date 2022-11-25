e9e765ce16fa9b433bf8aea9f4c52b326370f7a0
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2022-11-25 13:48:10 +1300

    add day 25

---
+++ day25/part1.go
@@ -0,0 +1,56 @@
+package day25
+
+import (
+	"aoc2021/util"
+	"fmt"
+)
+
+func Part1(inputFile string) string {
+	lines := util.ReadInput(inputFile)
+	sea := make([][]rune, len(lines))
+	for i, l := range lines {
+		sea[i] = []rune(l)
+		for j := range sea[i] {
+			if sea[i][j] == '.' {
+				sea[i][j] = 0
+			}
+		}
+	}
+	steps := 0
+	for moved := true; moved; steps++ {
+		moved = false
+
+		nextSea := make([][]rune, len(sea))
+		for i := range nextSea {
+			nextSea[i] = make([]rune, len(sea[0]))
+		}
+		for i, l := range sea {
+			for j, c := range l {
+				if c == '>' && l[(j+1)%len(l)] == 0 {
+					moved = true
+					nextSea[i][(j+1)%len(l)] = c
+				} else if c != 0 {
+					nextSea[i][j] = c
+				}
+			}
+		}
+		sea = nextSea
+
+		nextSea = make([][]rune, len(sea))
+		for i := range nextSea {
+			nextSea[i] = make([]rune, len(sea[0]))
+		}
+		for i, l := range sea {
+			for j, c := range l {
+				if c == 'v' && sea[(i+1)%len(sea)][j] == 0 {
+					moved = true
+					nextSea[(i+1)%len(sea)][j] = c
+				} else if c != 0 {
+					nextSea[i][j] = c
+				}
+			}
+		}
+		sea = nextSea
+	}
+	return fmt.Sprint(steps)
+}
